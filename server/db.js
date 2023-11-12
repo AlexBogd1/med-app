3a140c4566b9edbed4b33a81365d9b563274e455
Aliaksandar Bahdanovich <7894664@mail.ru>
2023-11-12 16:15:29 +0100

    add pas

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTk5MzktYm9nZGFu@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:NjkwNi1ib2dkYW5v@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
